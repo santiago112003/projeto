236dc475e0819e066aeab83bd83b11b6227ac1ab
santiago112003 <vs1311@outlook.com>
2024-09-07 23:39:20 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo: "Biografia",
+        descricao: "Angenor de Oliveira, mais conhecido por Cartola foi um cantor, compositor, poeta e violonista brasileiro. É considerado por diversos músicos e críticos musicais como o maior sambista da história da música brasileira. Tendo como seus principais sucessos as músicas As Rosas não Falam, O Mundo É um Moinho e Alvorada. Ajudou na fundação da agremiação Mangueira. Cartola passou a infância no bairro de Laranjeiras (Rio de Janeiro). Na infância conheceu a música e o samba, aprendendo violão com o pai. Com dificuldades financeiras sua numerosa família foi obrigada a mudar para o Morro da Mangueira, a nascente favela, onde fez amizade com Carlos Cachaça e outros bambas, além de se iniciar no mundo da boemia, da malandragem e do samba Após a morte de sua mãe abandonou os estudos — terminou apenas o primário. Virou servente de obra e passou a usar um chapéu-coco para se proteger do cimento que caía de cima. Por usar esse chapéu, ganhou dos colegas de trabalho o apelido Cartola. Junto com amigos sambistas do morro, Cartola criou o Bloco dos Arengueiros, cujo núcleo em 1928 fundou a Estação Primeira de Mangueira. Ele compôs também o primeiro samba para a escola de samba, Chega de Demanda. Suas composições se popularizaram na década de 1930 nas vozes ilustres de: Araci de Almeida; Carmen Miranda; Francisco Alves; Mário Reis e Sílvio Caldas. Mas como cantor apenas produziu o primeiro disco na década de 1970 (seis anos antes de falecer).Em 1974, aos 66 anos, Cartola gravou o primeiro de seus quatro discos-solo, e sua carreira tomou um novo impulso com clássicos instantâneos como: As Rosas não Falam; O Mundo É um Moinho; Acontece; O Sol Nascerá (com Elton Medeiros); Quem Me Vê Sorrindo (com Carlos Cachaça); Cordas de Aço; Alvorada, e; Alegria. No final da década de 1970, mudou-se para o bairro Jacarepaguá, onde morou até a morte, em 1980.",
+        link: "https://pt.wikipedia.org/wiki/Cartola_(compositor)",
+        tags: "historia vida bio sobre quem foi biografia"
+    },
+    {
+        titulo: "Musicas Clasicas",
+        descricao: "O Mundo É Um Moinho, Preciso Me Encontrar, As Rosas Não Falam, Alvorada, Acontece, Corra e Olhe o Céu, O Sol Nascerá, Peito Vazio, Que Sejas Bem Feliz, Disfarça E Chora, Cordas de Aço, A Cor da Esperança, A Pátria, Sala de Recepção, O Inverno do Meu Tempo, Meu Drama (Senhora Tentação), Amor Proibido, Deixa Pra Lá, Minha",
+        link: "https://www.letras.mus.br/cartola/",
+        tags: "musicas conções concoes mu musi musica"
+    },
+    {
+        titulo: "Final da Sua vida",
+        descricao: "Em 1980, Cartola viria a morrer em decorrência de um câncer. Em seu enterro, uma pequena multidão cantou o primoroso samba-canção “As rosas não falam”, acompanhada pelo bumbo do ritmista da Mangueira, Waldomiro, como o mestre mangueirense havia pedido. Cartola pedira também para ser lembrado pela clássica “O mundo é um moinho” e “O inverno de meu tempo”. Nascimento	11 de outubro de 1908, morte em 30 de novembro de 1980 (72 anos)",
+        link: "https://sites.usp.br/portalatinoamericano/espanol-cartola-2#:~:text=Em%201980%2C%20Cartola%20viria%20a,o%20mestre%20mangueirense%20havia%20pedido.",
+        tags: "morte fim de vida como moreeu se foi seu fim"
+    }
+    
+
+
+];
